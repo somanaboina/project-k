0317f6eb416ef9e433330920709e331d36f86a78
somanaboina <ilaiah.soma@gmail.com>
2024-01-14 00:41:12 -0500

    initial commit for eks jenkins project

---
+++ jenkins-tf/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-somana"
+        region = "us-east-1"
+        key    = "network/terraform.tfstate"
+        encrypt = "true"
+
+    }
+    required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
---
+++ jenkins-tf/iam-policy.tf
@@ -0,0 +1,4 @@
+resource "aws_iam_role_policy_attachment" "iam-policy" {
+    role = aws_iam_role.iam-role.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+} 
