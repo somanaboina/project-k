4c85ef70b429d41d01bd9b8dc6bf8189e617dd16
somanaboina <ilaiah.soma@gmail.com>
2024-01-14 18:26:33 -0500

    corrected routetable syntax

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -36,7 +36,7 @@ resource "aws_route_table" "rt2" {
     vpc_id = data.aws_vpc.vpc.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = data.aws_internet_gateway.igw
+        gateway_id = data.aws_internet_gateway.igw.id
     }
     tags = { 
       Name = var.rt-name2
