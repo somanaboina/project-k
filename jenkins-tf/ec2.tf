95f76bc954bf8b74b4b7376ebb06a25fe378c5d6
somanaboina <ilaiah.soma@gmail.com>
2024-02-02 17:40:45 -0500

    instance type change

--- jenkins-tf/ec2.tf
+++ jenkins-tf/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
     ami           = data.aws_ami.ami.image_id
-    instance_type = "t2.micro"
+    instance_type = "t2.large"
     key_name      =  "eks-prep"
     subnet_id     =  aws_subnet.public-subnet.id
     vpc_security_group_ids = [aws_security_group.security-group.id]
